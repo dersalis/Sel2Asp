f54f679023a2da726ea69c0da65f8a8a339414f3
Damian Ruta <damian.ruta@hotmail.com>
2017-08-18 15:12:52 +0200

    Changes in IISExpressManager

--- Sel2Asp.IISExpressManager/IISExpressManager.cs
+++ Sel2Asp.IISExpressManager/IISExpressManager.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Sel2Asp.IISExpressManager
 {
@@ -11,6 +8,67 @@ namespace Sel2Asp.IISExpressManager
     /// </summary>
     public class IISExpressManager
     {
-        
+        private int _port; // Port IIS [1024]
+        private string _applicationPath; // Scieżka katalogu Debug ["C:\PROJECTS\OnePage\OnePage.Web"]
+        private Process _process; // Proces IIS
+
+        private string _url; // Url do aplikacji
+        /// <summary>
+        /// Url do aplikacji
+        /// </summary>
+        public string Url
+        {
+            get
+            {
+                return String.Format($"http://localhost:{_port}");
+            }
+        }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="applicationPath">Ścieżka do plików</param>
+        /// <param name="port">Port IIS</param>
+        public IISExpressManager(string applicationPath, int port = 1024)
+        {
+            _port = port;
+            _applicationPath = applicationPath;
+        }
+
+        /// <summary>
+        /// Uruchamia serwer IIS Express
+        /// </summary>
+        public void Start()
+        {
+            var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+
+            _process = new Process();
+            _process.StartInfo.FileName = programFiles + @"\IIS Express\iisexpress.exe";
+            _process.StartInfo.Arguments = string.Format($"/path:\"{_applicationPath}\" /port:{_port}");
+            _process.Start();
+        }
+
+        /// <summary>
+        /// Zatrzymuje serwer IIS Express
+        /// </summary>
+        public void Stop()
+        {
+            if (_process.HasExited == false)
+                _process.Kill();
+        }
+
+        /// <summary>
+        /// Zwraca bezwzględny adres url
+        /// </summary>
+        /// <param name="relativeUrl">Względny adres url</param>
+        /// <returns>Bezwzględny adres url</returns>
+        public string GetAbsoluteUrl(string relativeUrl)
+        {
+            if (!relativeUrl.StartsWith("/"))
+            {
+                relativeUrl = "/" + relativeUrl;
+            }
+            return String.Format("http://localhost:{0}/{1}", _port, relativeUrl);
+        }
     }
 }
