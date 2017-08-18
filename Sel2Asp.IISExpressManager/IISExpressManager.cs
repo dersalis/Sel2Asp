57b5e7937de99d4d837a61a5f946c20a1ee5a0d4
Damian Ruta <Damian Ruta>
2017-08-18 14:56:05 +0200

    Add project files.

---
+++ Sel2Asp.IISExpressManager/IISExpressManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sel2Asp.IISExpressManager
+{
+    /// <summary>
+    /// Obsługuje server IIS Expres (dla programistów)
+    /// </summary>
+    public class IISExpressManager
+    {
+        
+    }
+}
